ce679db797a9d1dbb01c0e24410f3e04c89d6b85
Uziel <uziel.oliveira@intercode.com.br>
2023-10-01 01:26:55 -0300

    Joomla! 4.0.3 Portuguese, Brazil pt-BR Language Pack (v1)

--- README.md
+++
@@ -1,2 +0,0 @@
-# Joomla-pt-BR
-Joomla pt-BR Traduzido Melhor. Joomla pt-BR Translated Better.
--- language/pt-BR/localise.php
+++ language/pt-BR/localise.php
@@ -1,46 +1,42 @@
 <?php
-
 /**
- * @package    Joomla.Language
+ * @pacote Joomla.Language
  *
- * @copyright  (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license GNU General Public License versão 2 ou superior; consulte LICENSE.txt
+*/
 
- * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
-
- * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
- */
-
-// phpcs:disable PSR1.Files.SideEffects
-\defined('_JEXEC') or die;
-// phpcs:enable PSR1.Files.SideEffects
+defined('_JEXEC') or die;
 
 /**
  * pt-BR localise class.
  *
  * @since  1.6
  */
-abstract class Pt_BRLocalise
+abstract class pt_BRLocalise
 {
-    /**
-     * Retorna os sufixos potenciais para um número específico de itens
-     *
-     * @param   integer  $count  O número de itens.
-     *
-     * @return  array  Um array de sufixos potenciais.
-     *
-     * @since   1.6
-     */
-    public static function getPluralSuffixes($count)
-    {
-        switch ($count) {
-            case 0:
-                return ['0'];
-
-            case 1:
-                return ['UM', '1'];
-        }
-
-        return ['OUTROS', 'MAIS'];
-    }
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   integer  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			return array('0');
+		}
+		elseif ($count == 1)
+		{
+			return array('ONE', '1');
+		}
+		else
+		{
+			return array('OTHER', 'MORE');
+		}
+	}
 }
